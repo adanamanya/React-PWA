a1d71ea68e73d4c1521df8ea2942acc394846f75
gilang lubis <adanamanyaa@gmail.com>
2020-01-13 05:23:00 +0700

    update url

--- src/app/constants/api.js
+++ src/app/constants/api.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 export const fetchData = async () => {
   try {
     const response = await axios.get(
-      'http://www.mocky.io/v2/5c9105cb330000112b649af8',
+      'https://www.mocky.io/v2/5c9105cb330000112b649af8',
     );
     const data = response.data;
     // console.log(data,'eheehhe');
